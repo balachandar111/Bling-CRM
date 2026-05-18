fee7968eb5760d39862191d9f35d10afc3cd6bb1
balachandar111 <balachandarsanthoshkumar111.com>
2026-05-18 11:45:20 +0530

    CRM-overall- deploy

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+const express =
+require("express");
+
+const cors =
+require("cors");
+
+const app = express();
+
+
+// ================= MIDDLEWARE =================
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+
+// ================= ROUTES =================
+
+app.use(
+  "/api/auth",
+  require("./routes/authRoutes")
+);
+
+app.use(
+  "/api/customers",
+  require("./routes/customerRoutes")
+);
+
+app.use(
+  "/api/users",
+  require("./routes/userRoutes")
+);
+
+app.use(
+  "/api/employees",
+  require("./routes/employeeRoutes")
+);
+
+
+// ================= TEST =================
+
+app.get("/", (req, res) => {
+
+  res.send(
+    "CRM Backend Running"
+  );
+
+});
+
+module.exports = app;
